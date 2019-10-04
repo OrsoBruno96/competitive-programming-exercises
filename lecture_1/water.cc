baab2426799c27ae241fcca3ce8a3ced9a3011f5
Fabio Zoratti <fabio.zoratti96@gmail.com>
2019-10-04 14:16:18 +0200

    Correct answer to water problem in lecture 1

--- lecture_1/water.cc
+++ lecture_1/water.cc
@@ -4,13 +4,6 @@
 
 using namespace std;
 
-using pii = pair<int, int>;  // <height, horizontal position>
-
-struct order_compare {
-  bool operator() (const pii& a, const pii& b) const {
-    return a.first > b.first;
-  }
-};
 
 int main(int argc, char* argv[]) {
   int TC = 0;
@@ -18,58 +11,39 @@ int main(int argc, char* argv[]) {
   while (TC--) {
     int N = 0;
     cin >> N;
-    vector<pii> columns;
+    vector<int> columns;
     columns.reserve(N);
-    {
-      int prec = -1, count = 0;
-      for (auto i = N; i > 0; i--) {
-        int appo;
-        cin >> appo;
-        if (prec != appo) {
-          columns.push_back(pii(appo, count));
-          prec = appo;
-          count++;
-        }
-      }
+    // Get all values to be used later.
+    for (auto i = N; i > 0; i--) {
+      int appo;
+      cin >> appo;
+      columns.push_back(appo);
     }
-    // This problems admits a greedy solution.
-    // If I sort all the columns by height, i can loop
-    // over them in descending order and fill a rectangle with
-    // height of the second.
-    const order_compare cusumano;
-    stable_sort(columns.begin(), columns.end(), cusumano);
-    int counter =
-      (columns[0].second == 0
-       || columns[0].second == (columns.size() - 1)) ?
-      1 : 0;
-    int volume = 0, i = 1;
-    for (const auto& i: columns) {
-      cout << "(" << i.second << ", " << i.first << ") ";
-    }
-    cout << '\n';
-    // Sorted in reverse order, i need to start from the end.
-    // Actually not the end but the one before the end.
-    auto prec = columns[0];
-    auto precprec = columns[0];
-    while (counter < 2) {
-      const int horiz = (abs(columns[i].second - prec.second) - 1);
-      if (horiz == 0) {
-        if ( ) {
-          volume -= columns[i].first;
-        }
+    int volume = 0;
+
+    // The tallest water level will be in the "middle",
+    // Meaning that there exist a point x, between 0 and N - 1
+    // where if I go from 0 to x or from N - 1 to x, the water level
+    // is always increasing.
+    // This is useful because I can simply keep track of the max for the
+    // two sides while advancing to the middle.
+    // Every time I find a new tallest, the water level (height_(s|d)x)
+    // will rise.
+    auto sx = columns.begin();
+    auto dx = columns.end() - 1;
+    int height_sx = 0, height_dx = 0;
+    while (sx != dx) {
+      if (*sx < *dx) {
+        height_sx = max(*sx, height_sx);
+        volume += height_sx - *sx;
+        sx++;
       } else {
-        precprec = prec;
-        prec = columns[i];
-        volume += columns[i].first * horiz;
+        height_dx = max(*dx, height_dx);
+        volume += height_dx - *dx;
+        dx--;
       }
-      if (columns[i].second == 0 || columns[i].second == (columns.size() - 1)) {
-        counter++;
-      }
-      i++;
-    }
-    while (i < columns.size()) {
-      volume -= columns[i].first;
-      i++;
+      // cout << *sx << ", " << *dx << ", (" << height_sx << ", " <<
+      //   height_dx << ")\n";
     }
     cout << volume << "\n";
   }
