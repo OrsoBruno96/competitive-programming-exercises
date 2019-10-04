e223d19cca2a21d3cc41513f570336835f6135b3
Fabio Zoratti <fabio.zoratti96@gmail.com>
2019-10-04 14:49:55 +0200

    First attempt to solution of sliding window. Unfortunately, TLE

---
+++ lecture_1/window.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+  std::ios_base::sync_with_stdio(false);
+  int TC = 0;
+  cin >> TC;
+  while (TC--) {
+    int N = 0, K = 0;
+    cin >> N >> K;
+    deque<int> max_q;
+    vector<int> all_values;
+    all_values.reserve(K);
+    for (auto i = 0; i < N; i++) {
+      int appo;
+      cin >> appo;
+      all_values.push_back(appo);
+      while (!max_q.empty() && max_q.back() <= i - K) {
+        max_q.pop_back();
+      }
+      while (!max_q.empty() && all_values[max_q.front()] <= appo) {
+        max_q.pop_front();
+      }
+      max_q.push_front(i);
+
+      if (i > K - 2) {
+        cout << all_values[max_q.back()] << " ";
+      }
+    }
+    cout << "\n";
+  }
+}
