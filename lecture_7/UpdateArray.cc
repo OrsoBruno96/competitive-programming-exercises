44bf42147284a22fc130678cf46b503573399e23
Fabio Zoratti <fabio.zoratti96@gmail.com>
2019-10-15 17:14:37 +0200

    Added explaination to UpdateArray solution

--- lecture_7/UpdateArray.cc
+++ lecture_7/UpdateArray.cc
@@ -12,6 +12,11 @@ int main(int argc, char* argv[]) {
     int n = 0, u = 0;
     cin >> n >> u;
     Fenwick<int> ft(n);
+    // I exploit the fact that a FT gives as an answer the prefix
+    // sum of a vector with a good complexity (O(log n)), and admits
+    // updates with complexity O(log n)
+
+    // u iterations, each with O(log n) complexity
     for (auto i = 0; i < u; i++) {
       int l = 0, r = 0, v = 0;
       cin >> l >> r >> v;
@@ -22,11 +27,16 @@ int main(int argc, char* argv[]) {
     }
     int q = 0;
     cin >> q;
+    // each query is O(log n) and there are q queries.
     for (int i = 0; i < q; i++) {
       int appo;
       cin >> appo;
       cout << ft.sum(appo) << "\n";
     }
+    // Space complexity is good because with a fenwick tree
+    // I do not need to store the original array, i can use only
+    // this data structure, so the space complexity is linear with
+    // the size of the array.
   }
   return 0;
 }
