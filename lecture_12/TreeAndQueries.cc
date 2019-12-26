b989cb56b23de327958a296ec89fd52a5800760f
Fabio Zoratti <fabio.zoratti96@gmail.com>
2019-12-26 15:40:04 +0100

    Working solution to TreeAndQueries. This should be the last problem in the todolist.

--- lecture_12/TreeAndQueries.cc
+++ lecture_12/TreeAndQueries.cc
@@ -1,7 +1,20 @@
 
 
+// First of all I produce a linearized version of my tree.
+// I save in auxiliary vectors a dfs of the tree
+// and pointers to the dfs that for each vertex point to
+// the beginning and the end of its subtree.
+
+// then i sort the queries as in Mo's algorithm,
+// using the dfs as segment and the pointers as start and
+// end of the query.
+
+// Done that, I simply apply Mo's algorithm to obtain the answer.
+
+
+
+
 #include <bits/stdc++.h>
-#include "Fenwick.h"
 using namespace std;
 
 using pii = pair<int, int>;
@@ -11,92 +24,60 @@ using vppi = vector<ppi>;
 using vi = vector<int>;
 
 
-void add(int i, const vi& colors, vi& color_counter,
-         Fenwick<int>& greater_than) {
-  const auto old = color_counter[colors[i]];
-  // cout << "Aggiungo vertice " << i << " di colore " << colors[i] << endl;
-  // cout << "Prima c'era " << old << " volte\n\n" << endl;
+inline void add(int i, const vi& colors, vi& color_counter,
+         vi& greater_than) {
   color_counter[colors[i]]++;
-  if (old >= 0) {
-    greater_than.add(old, -1);
-  }
-  if (old >= -1) {
-    greater_than.add(old + 1, 1);
-  }
+  greater_than[color_counter[colors[i]]]++;
 }
 
 
-void remove(int i, const vi& colors, vi& color_counter,
-            Fenwick<int>& greater_than) {
-  const auto old = color_counter[colors[i]];
-  // cout << "Tolgo vertice " << i << " di colore " << colors[i] << endl;
-  // cout << "Prima c'era " << old << " volte\n\n" << endl;
+inline void remove(int i, const vi& colors, vi& color_counter,
+            vi& greater_than) {
+  greater_than[color_counter[colors[i]]]--;
   color_counter[colors[i]]--;
-  if (old >= 0) {
-    greater_than.add(old, -1);
-  }
-  if (old > 0) {
-    greater_than.add(old - 1, 1);
-  }
 }
 
 
-void solve(const vppi& queries, const vi& euler_tour, const vi& colors,
+void solve(const vppi& queries, const vi& visit, const vi& colors,
            vi& answers, const vpii& terminals) {
   int curr_i = 0, curr_j = 0;
   const int n = colors.size();
-  vi color_counter(n + 1, 0);
-  Fenwick<int> greater_than(2*n);
-  greater_than.add(0, n);
+  vi color_counter(n, 0);
+  vi greater_than(n + 2, 0);
 
   add(0, colors, color_counter, greater_than);
   for (const auto& it: queries) {
     const auto query = it.second;
     const auto v_i = query.first;
     const auto k_i = query.second;
-    // cout << "Query: subtree di " << v_i << " " << k_i << " volte" << endl;
-    // cout << "Voglio raggiungere a sx " << terminals[v_i].first <<
-    //   " e a dx " << terminals[v_i].second << endl;
+
+    if (k_i > n + 1) {
+      answers[it.first] = 0;
+      continue;
+    }
 
     while (curr_j > terminals[v_i].second) {
-      remove(euler_tour[curr_j], colors, color_counter, greater_than);
+      remove(visit[curr_j], colors, color_counter, greater_than);
       curr_j--;
     }
     while (curr_j < terminals[v_i].second) {
       curr_j++;
-      add(euler_tour[curr_j], colors, color_counter, greater_than);
+      add(visit[curr_j], colors, color_counter, greater_than);
     }
     while (curr_i < terminals[v_i].first) {
-      remove(euler_tour[curr_i], colors, color_counter, greater_than);
+      remove(visit[curr_i], colors, color_counter, greater_than);
       curr_i++;
     }
     while (curr_i > terminals[v_i].first) {
       curr_i--;
-      add(euler_tour[curr_i], colors, color_counter, greater_than);
+      add(visit[curr_i], colors, color_counter, greater_than);
     }
     auto ans = 0;
-    if (k_i > 0) {
-      if (k_i < n + 2) {
-        ans = n - greater_than.sum(k_i - 1);
-      } else {
-        ans = 0;
-      }
+    if (k_i >= 0) {
+      ans = greater_than[k_i];
     } else {
       ans = n;
     }
-    // cout << "Sono arrivato in posizione corretta. Dumpo i colori" << endl;
-    // for (auto it: color_counter) {
-    //   cout << it << " ";
-    // }
-    // cout << endl;
-    // cout << "Quelli che sono piú piccoli di " << k_i - 1 << " sono " <<
-    //   greater_than.sum(k_i - 1) << endl;
-    // cout << "Risposta a questa query: " << ans << endl;
-    // cout << "Dumpo il FT "  << endl;
-    // for (int i = 0; i < n; i++) {
-    //   cout << greater_than[i] << " ";
-    // }
-    // cout << endl;
     answers[it.first] = ans;
   }
 }
@@ -128,15 +109,14 @@ void get_sorted_queries(const int division, const int t,
     cin >> appo1 >> appo2;
     appo1--;  // We want zero indexed values
     int group = groups[appo1];
-    // cout << "Putting query for " << terminals[appo1].first << ", " <<
-    //   terminals[appo1].second << "in group " << group << endl;
     query_groups[group].emplace_back(ppi(i, pii(appo1, appo2)));
   }
+
   for (auto& it: query_groups) {
     std::sort(it.begin(), it.end(),
               [&terminals](const ppi& left, const ppi& right) {
-                return terminals[left.second.first] <
-                  terminals[right.second.first];
+                return terminals[left.second.first].second <
+                  terminals[right.second.first].second;
               });
   }
   for (const auto& it: query_groups) {
@@ -148,25 +128,24 @@ void get_sorted_queries(const int division, const int t,
 
 
 void dfs(const int vertex, const vector<vi>& adj_list,
-         vi& euler_tour, vector<bool>& visited,
+         vi& visit, vector<bool>& visited,
          vpii& terminals) {
   if (visited[vertex]) { return; }
   visited[vertex] = true;
-  terminals[vertex].first = euler_tour.size();
-  euler_tour.push_back(vertex);
+  terminals[vertex].first = visit.size();
+  visit.push_back(vertex);
   for (const auto& it: adj_list[vertex]) {
-    dfs(it, adj_list, euler_tour, visited, terminals);
+    dfs(it, adj_list, visit, visited, terminals);
   }
-  terminals[vertex].second = euler_tour.size() - 1;
-  // euler_tour.push_back(vertex);
+  terminals[vertex].second = visit.size() - 1;
 }
 
 
 
-void produce_euler_tour(const int n, const vector<vi>& adj_list,
-                        vi& euler_tour, vpii& terminals) {
+void produce_visit(const int n, const vector<vi>& adj_list,
+                        vi& visit, vpii& terminals) {
   vector<bool> visited(n + 1, false);
-  dfs(0, adj_list, euler_tour, visited, terminals);
+  dfs(0, adj_list, visit, visited, terminals);
 }
 
 
@@ -199,13 +178,13 @@ int main() {
   cin >> n >> m;
   vi colors(n);
   vector<vi> adj_list(n, vi());
-  vi euler_tour;
+  vi visit;
   // Pair of values. For every node n in the tree
-  // terminals[n] is a pair of indexes in euler_tour
+  // terminals[n] is a pair of indexes in visit
   // that point to the beginning and to the end of the
   // subtree.
   vpii terminals(n, pii(-1, -1));
-  euler_tour.reserve(n);
+  visit.reserve(n);
   terminals.reserve(n);
   for (auto i = 0; i < n; i++) {
     cin >> colors[i];
@@ -215,34 +194,10 @@ int main() {
   for (auto i = 0; i < n - 1; i++) {
     int appo1 = 0, appo2 = 0;
     cin >> appo1 >> appo2;
-    appo1--;
-    appo2--;
-    adj_list[appo1].push_back(appo2);
-    adj_list[appo2].push_back(appo1);
+    appo1--; appo2--;
+    adj_list[appo1].push_back(appo2); adj_list[appo2].push_back(appo1);
   }
-  produce_euler_tour(n, adj_list, euler_tour, terminals);
-  // for (int i = 0; i < adj_list.size(); i++) {
-  //   cout << "Vertex " << i + 1 << ": ";
-  //   for (auto it: adj_list[i]) {
-  //     cout << it + 1 << " ";
-  //   }
-  //   cout << endl;
-  // }
-  // cout << "Euler tour: " << endl;
-  // for (auto it: euler_tour) {
-  //   cout << it + 1 << " ";
-  // }
-  // cout << endl;
-  // cout << "Terminals: " << endl;
-  // for (auto it: terminals) {
-  //   cout << "(" << it.first << ", " << it.second << ") ";
-  // }
-  // cout << endl;
-  // for (int i = 0; i < colors.size(); i++) {
-  //   cout << colors[i] << " ";
-  // }
-  // cout << endl;
-
+  produce_visit(n, adj_list, visit, terminals);
 
   vppi queries;
   vector<int> answers(m, 0);
@@ -250,13 +205,8 @@ int main() {
   const int division = std::sqrt(n) + 1;
   get_sorted_queries(division, m, queries, terminals);
 
-  // cout << "Debugging query order" << endl;
-  // for (auto it: queries) {
-  //   cout << "(" << it.second.first << ", " << it.second.second << ") ";
-  // }
-  // cout << endl;
 
-  solve(queries, euler_tour, colors, answers, terminals);
+  solve(queries, visit, colors, answers, terminals);
   for (const auto& it: answers) {
     cout << it << "\n";
   }
